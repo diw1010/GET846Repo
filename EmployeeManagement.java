ce8adbee4697b4038e9c406ab51bd42a99d98c96
diwsharma <31534746+diw1010@users.noreply.github.com>
2020-08-05 15:01:37 +0530

    Calculation module added

--- EmployeeManagement.java
+++ EmployeeManagement.java
@@ -2,4 +2,11 @@ class Hello{
   System.out.println("Hello world!");
   System.out.println("Hello again");
 
+  
+  public void add(int a, int b){
+    System.out.println("Add"+(a+b));
+  }
+  public void diff(int a, int b){
+    System.out.println("Difference"+(a-b));
+  }
 }
